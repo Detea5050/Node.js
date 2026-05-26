86179f0d8d827c3fdd49bdb91cd87fb62bce3a66
detea5050 <detea5050@gmail.com>
2026-05-26 15:01:07 +0100

    added new file

--- echo.js
+++ echo.js
@@ -1,6 +1,15 @@
 const args = process.argv.slice(2);
 
 args.forEach((arg) => {
+    
+    console.log(process.env[arg]);
+
+});
+
+
+const args = process.argv.slice(2);
+
+args.forEach((arg) => {
 
 
     if (process.env[arg]) {
